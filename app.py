2597d896436cdd5e82fd1c87ca6981004c6831ae
theoclark <103810963+theoclark@users.noreply.github.com>
2022-06-27 18:19:20 +0300

    clean up code

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from time import time, sleep
 import torch
 from torch import nn
-import numpy as np
-import cv2
 from torchvision import transforms
+import cv2
+
+# Adjustable Parameters
+frame_size = 420                # Frame size for input and output boxes
+image_size = 256                # Image size for the model
+buffer_size = 30                # Number of previous predictions to keep in buffer
+threshold = 0.3                 # Threshold above which to display prediction
+input_x, input_y = 20, 250      # Top-left coordinate for the input box
+output_x, output_y = 800, 250   # Top-left coordinate for the output box
 
 class Model(nn.Module):
+    """
+    ConvNet for sign recognition.
+    """
     def __init__(self):
         super().__init__()
         self.convnet = nn.Sequential(
@@ -52,60 +61,58 @@ class Model(nn.Module):
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 weights_path = './model_weights.pt'
-frame_size = 420
-image_size = 256
-buffer_size = 30
-threshold = 0.3
-input_x, input_y = 20, 250
-output_x, output_y = 800, 250
 alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 buffer = torch.zeros((buffer_size, 26)).to(device)
 model = Model()
 model.load_state_dict(torch.load(weights_path, map_location=torch.device(device)))
-upsample = nn.Upsample((frame_size,frame_size))
-downsample = transforms.Resize((image_size,image_size))
+resize = transforms.Resize((image_size,image_size))
 
 def score_frame(frame):
+    """
+    Crops the frame to the input box size and passes it to the model.
+    Returns the predicted values for each letter fed through softmax.
+    """
     with torch.no_grad():
-        frame = torch.tensor(frame).permute((2,0,1)).type(torch.uint8)
-        cropped_frame = transforms.functional.crop(frame,input_y,input_x,frame_size,frame_size),
-        pred_frame = downsample(cropped_frame[0])
-        image = pred_frame.float()/255
+        image = torch.tensor(frame).permute((2,0,1)).type(torch.uint8)
+        image = transforms.functional.crop(image,input_y,input_x,frame_size,frame_size),
+        image = resize(image[0])
+        image = image.float()/255
         logits = model(image.to(device))
         pred = nn.functional.softmax(logits,dim=-1)
-        return pred, pred_frame
+        return pred
 
 def generate_prediction(frame, buffer):
-    pred, pred_frame = score_frame(frame)
-    idx = pred.argmax()
-    letter = alphabet[idx]
+    """
+    Adds the latest prediction to the buffer (removing the oldest value).
+    Buffer is then averaged and the maximum value returned.
+    If the predicted value is above the threshold the prediction is displayed.
+    """
+    pred = score_frame(frame)
     buffer = torch.cat((buffer[1:],pred.unsqueeze(0)))
     mean_buffer = buffer.mean(dim=0)
     prob, idx = mean_buffer.max(0)
     if prob > threshold:
-      return pred_frame, buffer, alphabet[idx]
+      return buffer, alphabet[idx]
     else:
-      return pred_frame, buffer, "/"
+      return buffer, "/"
 
+# Open input video stream
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
     raise IOError("Cannot open webcam")
 
 with torch.no_grad():
     while True:
+        # Get current frame
         ret, frame = cap.read()
-        pred_frame, buffer, pred = generate_prediction(frame, buffer)
-        frame = torch.tensor(frame).unsqueeze(0).cpu().numpy()
-        frame[:,output_y:output_y+frame_size,output_x:output_x+frame_size,:] = 0
-        frame = frame.squeeze()
-        white = (255,255,255)
+        # Generate prediction
+        buffer, pred = generate_prediction(frame, buffer)
+        # Edit and show output frame
+        frame[output_y:output_y+frame_size,output_x:output_x+frame_size,:] = 0
+        rgb = (255,255,255)
         label_font = cv2.FONT_HERSHEY_SIMPLEX
-        frame = cv2.putText(frame, pred, (output_x+100, output_y+frame_size-100), label_font, 8, white, 10)
-        frame = cv2.rectangle(frame,(input_x, input_y), (input_x+frame_size, input_y+frame_size), white, 2)
+        frame = cv2.putText(frame, pred, (output_x+100, output_y+frame_size-100), label_font, 8, rgb, 10)
+        frame = cv2.rectangle(frame,(input_x, input_y), (input_x+frame_size, input_y+frame_size), rgb, 2)
         frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
-        cv2.imshow('Input', frame)
+        cv2.imshow('Sign Language Detector', frame)
         c = cv2.waitKey(1)
-        if c == 27:
-            break
-    cap.release()
-    cv2.destroyAllWindows()
